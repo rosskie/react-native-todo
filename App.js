5f16e7ababeea4ccca7a5b08726e15dda1195c4f
Rossa <roe.utena@gmail.com>
2022-02-06 00:05:34 +0900

    :monkey:

--- App.js
+++ App.js
@@ -1,20 +1,129 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler'; //!important: must be written on top
+
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import MyDrawer from './routes/MyDrawer';
+import store from './store/store';
+import { Provider } from 'react-redux';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <Provider store={store}>
+      <NavigationContainer>
+        <MyDrawer />
+      </NavigationContainer>
+    </Provider>
+  )
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+
+
+//StatusBar Component Example
+
+// import React, { useState } from 'react';
+// import { Button, Platform, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
+
+// const STYLES = ['default', 'dark-content', 'light-content'];
+// const TRANSITIONS = ['fade', 'slide', 'none'];
+
+// const App = () => {
+//   const [hidden, setHidden] = useState(false);
+//   const [statusBarStyle, setStatusBarStyle] = useState(STYLES[0]);
+//   const [statusBarTransition, setStatusBarTransition] = useState(TRANSITIONS[0]);
+
+//   const changeStatusBarVisibility = () => setHidden(!hidden);
+
+//   const changeStatusBarStyle = () => {
+//     const styleId = STYLES.indexOf(statusBarStyle) + 1;
+//     if (styleId === STYLES.length) {
+//       setStatusBarStyle(STYLES[0]);
+//     } else {
+//       setStatusBarStyle(STYLES[styleId]);
+//     }
+//   };
+
+//   const changeStatusBarTransition = () => {
+//     const transition = TRANSITIONS.indexOf(statusBarTransition) + 1;
+//     if (transition === TRANSITIONS.length) {
+//       setStatusBarTransition(TRANSITIONS[0]);
+//     } else {
+//       setStatusBarTransition(TRANSITIONS[transition]);
+//     }
+//   };
+
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <StatusBar
+//         animated={true}
+//         backgroundColor="#61dafb"
+//         barStyle={statusBarStyle}
+//         showHideTransition={statusBarTransition}
+//         hidden={hidden} />
+//       <Text style={styles.textStyle}>
+//         StatusBar Visibility:{'\n'}
+//         {hidden ? 'Hidden' : 'Visible'}
+//       </Text>
+//       <Text style={styles.textStyle}>
+//         StatusBar Style:{'\n'}
+//         {statusBarStyle}
+//       </Text>
+//       {Platform.OS === 'ios' ? (
+//         <Text style={styles.textStyle}>
+//           StatusBar Transition:{'\n'}
+//           {statusBarTransition}
+//         </Text>
+//       ) : null}
+//       <View style={styles.buttonsContainer}>
+//         <Button
+//           title="Toggle StatusBar"
+//           onPress={changeStatusBarVisibility} />
+//         <Button
+//           title="Change StatusBar Style"
+//           onPress={changeStatusBarStyle} />
+//         {Platform.OS === 'ios' ? (
+//           <Button
+//             title="Change StatusBar Transition"
+//             onPress={changeStatusBarTransition} />
+//         ) : null}
+//       </View>
+//     </SafeAreaView>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     backgroundColor: '#ECF0F1'
+//   },
+//   buttonsContainer: {
+//     padding: 10
+//   },
+//   textStyle: {
+//     textAlign: 'center',
+//     marginBottom: 8
+//   }
+// });
+
+// export default App;
